8151f158606a3b27c424f16a0de11413266c2703
Stanislav Volokhov <lamantina123@gmail.com>
2022-10-03 23:57:21 +0400

    add task#2

---
+++ Task#2.py
@@ -0,0 +1,18 @@
+# Задание 2 Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# Пример:
+
+# пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+N = int(input('Input N: '))
+count = 0
+lst = []
+start = 1
+step = 2
+while count < N:
+    lst.append(start)
+    start = start*step
+    count += 1
+    step += 1
+print(lst)
+print(f'Products of numbers from 1 to {N} =', lst)
